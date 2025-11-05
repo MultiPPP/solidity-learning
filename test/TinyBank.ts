46bed1ffbf6d28eabcd617efd7c525b62df591a9
MultiPPP <mulpb01@gmail.com>
2025-11-06 06:44:31 +0900

    add reward test

--- test/TinyBank.ts
+++ test/TinyBank.ts
@@ -57,4 +57,26 @@ describe("TinyBank", () => {
       expect(await TinyBankC.staked(signer0.address)).equal(0);
     });
   });
+
+  describe("reward", () => {
+    it("should reward 1MT every blocks", async () => {
+      const signer0 = signers[0];
+      const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
+      await myTokenC.approve(await TinyBankC.getAddress(), stakingAmount);
+      await TinyBankC.stake(stakingAmount);
+
+      // 5개의 block동안 reward 받고, block number 증가, withdraw할 때 block number 1 더 증가
+      // 5n, 1n: Big Number
+      const BLOCKS = 5n;
+      const transferAmount = hre.ethers.parseUnits("1", DECIMALS);
+      for (var i = 0; i < BLOCKS; i++) {
+        await myTokenC.transfer(transferAmount, signer0.address);
+      }
+
+      await TinyBankC.withdraw(stakingAmount);
+      expect(await myTokenC.balanceOf(signer0.address)).equal(
+        hre.ethers.parseUnits((BLOCKS + MINTING_AMOUNT + 1n).toString())
+      );
+    });
+  });
 });
